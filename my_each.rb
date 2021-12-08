8750924ca0982c4b7e17b4a44b6cb831e5ea4f72
Ray's Git <lancelot53.ra@gmail.com>
2021-12-08 18:14:01 -0500

    Completed

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,20 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(pizza)
+  i = 0
+  while i < pizza.length
+    yield(pizza[i])
+    i = i + 1
+  end
+  pizza
 end
+
+# code here
+
+
+# def my_each(collection)
+#   i = 0
+#   while i < collection.length
+#     yield(collection[i])
+#     i = i + 1
+#   end
+#   collection
+# end
